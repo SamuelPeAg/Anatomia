c3a5d265f0829c3bd70fcc640b1d597140ffffc8
SamuelPeAg <spa0004@alu.medac.es>
2026-02-19 14:02:39 +0100

    quitando archivos que no sirven

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
                 'resources/css/principal.css',
                 'resources/js/principal.js',
                 'resources/css/nuevoinforme.css',
